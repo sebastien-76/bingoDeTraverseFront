a4cde33eecbf0a03222a5f103ec2f1ebd9f75977
sebastien <sebastien.divers@gmail.com>
2024-07-25 17:12:14 +0200

    SMA/feat/profile

--- src/pages/users/profil/profil.jsx
+++ src/pages/users/profil/profil.jsx
@@ -5,6 +5,7 @@ import './profil.css';
 const Profile = (props) => {
     const id = useParams();
     const [profil, setProfil] = useState([]);
+    const [openModalModif, setOpenModalModif] = useState(false);
 
     const fetchProfil = async (id) => {
         const response = await fetch(`http://localhost:3000/api/users/${id}`);
@@ -13,25 +14,63 @@ const Profile = (props) => {
     }
 
     useEffect(() => {
+        fetchProfil(id.id)
+    }, [id]);
+
+    const onChange = (event) => setProfil({ ...profil, [event.target.name]: event.target.value })
+
+    const onSubmitModifProfile = async (event) => {
+        event.preventDefault();
+        await fetch(`http://localhost:3000/api/users/${id.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(profil)
+        });
         fetchProfil(id.id);
-    }, [id])
-
-    const onChange = (event) => setProfil({ ...profil, [event.target.name]: event.target.value }) 
-
+        setOpenModalModif(false);
+        console.log(profil)
+    }
 
     return (
         <div className="profil">
             <h1>Profil</h1>
-            <form className="formProfil">
+            <form id="formProfil" className="formProfil">
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" value={profil.email}  onChange={onChange}/>
+                <input type="text" id="email" name="email" defaultValue={profil.email} />
                 <label htmlFor="lastname">Nom</label>
-                <input type="text" id="lastname" name="lastname" value={profil.lastname} onChange={onChange} />
+                <input type="text" id="lastname" name="lastname" defaultValue={profil.lastname} />
                 <label htmlFor="firstname">Prénom</label>
-                <input type="text" id="firstname" name="firstname" value={profil.firstname} onChange={onChange}/>
+                <input type="text" id="firstname" name="firstname" defaultValue={profil.firstname} />
                 <label htmlFor="pseudo">Pseudo</label>
-                <input type="text" id="pseudo" name="pseudo" value={profil.pseudo} onChange={onChange} />
+                <input type="text" id="pseudo" name="pseudo" defaultValue={profil.pseudo} />
+                { (profil.Salles && profil.Salles.length>0) ? <h3>Salles associées :</h3> : <p>Aucune salle associée</p>}
+                <ul>
+                    {profil.Salles && profil.Salles.map(salle =>
+                        <li key={salle.id} className="salleList">{salle.name}</li>)}
+                </ul>
             </form>
+
+            <button onClick={() => setOpenModalModif(true)}>Modifier</button>
+
+            {openModalModif ? 
+                <form id="formProfilModif" onSubmit={onSubmitModifProfile}  className="formProfilModif">
+                <h2>Modifier les informations</h2>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" defaultValue={profil.email} onChange={onChange} />
+                <label htmlFor="lastname">Nom</label>
+                <input type="text" id="lastname" name="lastname" defaultValue={profil.lastname} onChange={onChange} />
+                <label htmlFor="firstname">Prénom</label>
+                <input type="text" id="firstname" name="firstname" defaultValue={profil.firstname} onChange={onChange} />
+                <label htmlFor="pseudo">Pseudo</label>
+                <input type="text" id="pseudo" name="pseudo" defaultValue={profil.pseudo} onChange={onChange} />
+                <label htmlFor="password">Mot de passe</label>
+                <input type="text" id="password" name="password" defaultValue="" autoComplete="off" onChange={onChange} />
+                <input type="submit" id="submit" value="Valider" />
+                </form>
+                : null}
+
 
         </div>
     )
