3af47b70a73955642bcf58306bd690424168e4fc
sebastien <sebastien.divers@gmail.com>
2024-08-06 15:56:34 +0200

    SMA/fix/accueil ajout logo

--- src/pages/users/accueil/accueil.jsx
+++ src/pages/users/accueil/accueil.jsx
@@ -6,9 +6,10 @@ const Accueil = () => {
 
     return (
         <>
+            <img src="../../../images/cheminTraverse.png" alt="logo chemin de tarverse" className="logo_accueil" />
             <h1 className="titre_accueil">Bienvenue sur le <br/><span>Bingo de traverse!</span></h1>
-            <p className="p_accueil">Pas encore inscrit, cliquer <Link to="/inscription" className="lien_accueil">içi</Link></p>
-            <p className="p_accueil">Pour vous connecter, c'est par <Link to="/connexion" className="lien_accueil">içi</Link></p>
+            <p className="p_accueil">Pas encore inscrit, cliquer <Link to="/inscription" className="lien_accueil">ici</Link></p>
+            <p className="p_accueil">Pour vous connecter, c'est par <Link to="/connexion" className="lien_accueil">ici</Link></p>
         </>
     )
 
