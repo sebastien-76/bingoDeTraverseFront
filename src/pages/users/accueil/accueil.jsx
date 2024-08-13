d45423b2a790ac298f8d97924156ea62c69c7609
sebastien <sebastien.divers@gmail.com>
2024-08-13 14:24:59 +0200

    SMA/fix/modification connexion et inscription accueil

--- src/pages/users/accueil/accueil.jsx
+++ src/pages/users/accueil/accueil.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import './accueil.css';
 
+import authContext from "../../../hooks/useAuth";
+
 const Accueil = () => {
+
+    const { estConnecte } = useContext(authContext);
 
     return (
         <>
             <img src="../../../images/cheminTraverse.png" alt="logo chemin de tarverse" className="logo_accueil" />
-            <h1 className="titre_accueil">Bienvenue sur le <br/><span>Bingo de traverse!</span></h1>
-            <button className="btn_connexion"><Link to="/connexion" className="btn_connexion_link">Connexion</Link></button>
-            <p className="p_accueil">Pas encore inscrit, c'est par <Link to="/inscription" className="lien_accueil">ici</Link></p>
+            <h1 className="titre_accueil">Bienvenue sur le <br /><span>Bingo de traverse!</span></h1>
+            {(!estConnecte) &&
+                <div>
+                    < button className="btn_connexion"><Link to="/connexion" className="btn_connexion_link">Connexion</Link></button >
+                    <p className="p_accueil">Pas encore inscrit, c'est par <Link to="/inscription" className="lien_accueil">ici</Link></p>
+                </div>
+            }
         </>
     )
 
