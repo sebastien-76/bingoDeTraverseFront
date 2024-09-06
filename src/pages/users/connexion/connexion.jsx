4a2ff8b06083d9279dd60f64f37d0572160b2012
Sebastien MAILLET <sebastien.maillet@gmail.com>
2024-09-06 14:27:47 +0200

    SMA/fix/inputs inscription connexion

--- src/pages/users/connexion/connexion.jsx
+++ src/pages/users/connexion/connexion.jsx
@@ -56,9 +56,9 @@ const Connexion = () => {
             {errorConnexion && <p className="error_connexion">{errorConnexion}</p>}
             <form id="form_connexion" onSubmit={handleSubmitConnexion} className="form_connexion">
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" value={credentials.email} onChange={onChange} placeholder="Entrez votre email" required />
+                <input type="email" id="email" name="email" className="pageInput" value={credentials.email} onChange={onChange} placeholder="Entrez votre email" required />
                 <label htmlFor="password">Mot de passe</label>
-                <input type="password" id="password" name="password" value={credentials.password} placeholder="Entrez votre mot de passe" autoComplete="off" onChange={onChange} required />
+                <input type="password" id="password" name="password" className="pageInput" value={credentials.password} placeholder="Entrez votre mot de passe" autoComplete="off" onChange={onChange} required />
             </form>
                 <Bouton text="Se connecter" style={{ height: '3em', marginTop: '2rem', width: '150px', backgroundColor: 'var(--purple-pastel)', border: '2px solid var(--purple-pastel)' }} onClick={handleSubmitConnexion} />
         </>
